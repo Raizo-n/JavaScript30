a1a2060a15be1b5439dc626379939131a778ae3e
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-02 23:57:08 -0700

    complete

--- 29-CountDownTimer/scripts.js
+++ 29-CountDownTimer/scripts.js
@@ -1,8 +1,11 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
 const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -34,6 +37,24 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:${
-    minutes <= 9 ? "0" : "" }${minutes}`;
+  // convert the time's display to 24-hour notion to 12-hour notation
+  endTime.textContent = `Be Back at ${hour > 12 ? hour - 12 : hour}:
+  ${minutes <= 9 ? "0" : ""}${minutes}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", startTimer);
+});
+
+document.customForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  console.log(mins);
+  timer(mins * 60);
+  this.reset();
+});
