afc7e68edc461905edfd7567fe81b3ab3f6bcb27
“Raizo-n” <sanpincha0826@gmail.com>
2023-04-06 23:57:39 -0700

    add: takePhoto func

--- 19-WebcamFun/scripts.js
+++ 19-WebcamFun/scripts.js
@@ -10,22 +10,34 @@ function getVideo() {
     .then((localMediaStream) => {
       console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
-    //   video.play();
+      //   video.play();
     })
     .catch((err) => {
       console.error(`Something went wrong...`, err);
     });
 }
 
-function paintToCanvas(){
-    const width = video.videoWidth;
-    const height = video.videoHeight;
-    canvas.width = width;
-    canvas.height = height;
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
-    return setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height)
-    }, 16)
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL("image/jpeg");
+  console.log(data);
 }
 
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
